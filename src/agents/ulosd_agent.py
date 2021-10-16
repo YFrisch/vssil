Fix for pc loss gradients

src/agents/ulosd_agent.py
@@ -158,7 +158,8 @@ class ULOSD_Agent(AbstractAgent):
                                           image_sequence=image_sequence,
                                           patch_size=eval(config['training']['pixelwise_contrastive_patch_size']),
                                           time_window=config['training']['pixelwise_contrastive_time_window'],
-                                          alpha=config['training']['pixelwise_contrastive_alpha']) * scale
+                                          alpha=config['training']['pixelwise_contrastive_alpha'],
+                                          patch_diff_mode=config['training']['pixelwise_contrastive_distance']) * scale
 
     def l1_activation_penalty(self, feature_maps: torch.Tensor, config: dict) -> torch.Tensor:
         feature_map_mean = torch.mean(feature_maps, dim=[-2, -1])
@@ -295,13 +296,13 @@ class ULOSD_Agent(AbstractAgent):
             tc_triplet_loss = torch.Tensor([0.0]).to(self.device)
 
         if config['training']['pixelwise_contrastive_scale'] > 0:
-            pixelwise_contrastive_loss = self.pixelwise_contrastive_loss(
+            pc_loss = self.pixelwise_contrastive_loss(
                 keypoint_coordinates=observed_key_points,
                 image_sequence=sample,
                 config=config
             )
         else:
-            pixelwise_contrastive_loss = torch.Tensor([0.0]).to(self.device)
+            pc_loss = torch.Tensor([0.0]).to(self.device)
 
         #
         # Losses for the dynamics model
@@ -316,7 +317,7 @@ class ULOSD_Agent(AbstractAgent):
         # total loss
         L = reconstruction_loss + separation_loss + l1_penalty +\
             coord_pred_loss + kl_loss +\
-            consistency_loss + tc_triplet_loss + pixelwise_contrastive_loss
+            consistency_loss + tc_triplet_loss + pc_loss
 
         if mode == 'validation' and config['validation']['save_video'] and save_val_sample:
             # NOTE: This part seems to cause a linear increase in CPU memory usage
@@ -340,7 +341,7 @@ class ULOSD_Agent(AbstractAgent):
             self.sep_loss_per_iter.append(separation_loss.item())
             self.cons_loss_per_iter.append(consistency_loss.item())  # Extension
             self.tc_loss_per_iter.append(tc_triplet_loss.item())  # Extension
-            self.pi_co_loss_per_iter.append(pixelwise_contrastive_loss.item())  # Extension
+            self.pi_co_loss_per_iter.append(pc_loss.item())  # Extension
             self.l1_penalty_per_iter.append(l1_penalty.item())
             self.total_loss_per_iter.append(L.item())
 

src/losses/pixelwise_contrastive_loss.py
@@ -30,7 +30,7 @@ def get_patch_by_gridsampling(keypoint_coordinates: torch.Tensor,
             grid[:, h_i, w_i, 0] = keypoint_coordinates[:, 0] + step_size_h * step_h
             grid[:, h_i, w_i, 1] = keypoint_coordinates[:, 1] + step_size_w * step_w
 
-    patches = F.grid_sample(input=image, grid=grid, align_corners=False)
+    patches = F.grid_sample(input=image, grid=grid, padding_mode='border', align_corners=False)
 
     assert tuple(patches.shape[-2:]) == patch_size, f'{tuple(patches.shape[-2:])} != {patch_size}'
     assert patches.dim() == 4

tests/pixelwise_contrastive_patch_loss_test.py
@@ -155,17 +155,11 @@ class PatchLossTest(unittest.TestCase):
 
         """
 
-        fake_fnn = torch.nn.Linear(in_features=20 * 3 * 2, out_features=20 * 3 * 2, bias=False)
-        fake_fnn2 = torch.nn.Linear(in_features=20 * 3 * 2, out_features=20 * 3 * 2, bias=False)
-
-        patch_size = (5, 5)
-        time_window = 9
-        alpha = 0.5
+        patch_size = (15, 15)
+        time_window = 7
+        alpha = 1.0
 
         fake_kpts = self.each_kp_diff_patch(self.fake_img_series)
-        print(fake_kpts.shape)
-        print(torch.flatten(fake_kpts, start_dim=1).shape)
-        fake_kpts = fake_fnn2(fake_fnn(torch.flatten(fake_kpts, start_dim=1))).view(1, 20, 3, 2)
         fake_kpts2 = self.two_kpts_same_patch(self.fake_img_series)
         fake_kpts3 = self.all_kpts_same_patch(self.fake_img_series)
 
@@ -213,9 +207,9 @@ class PatchLossTest(unittest.TestCase):
 
         """
 
-        patch_size = (5, 5)
-        time_window = 9
-        alpha = 0.5
+        patch_size = (15, 15)
+        time_window = 7
+        alpha = 1.0
 
         fake_kpts = self.each_kp_diff_patch(self.fake_img_series)
         fake_kpts4 = self.kpts_changing_patches(self.fake_img_series)