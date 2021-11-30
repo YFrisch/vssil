d45c03efeac2dc4f27ff7076fed51679289f92bc
yannik <yannik_phil.frisch@stud.tu-darmstadt.de>
2021-11-30 20:19:09 +0100

    refactor

--- src/losses/perception_encoding_loss.py
+++ src/losses/perception_encoding_loss.py
@@ -5,15 +5,11 @@ import torch.nn.functional as F
 def perception_loss(perception_net: torch.nn.Module,
                     prediction: torch.Tensor,
                     target: torch.Tensor) -> torch.Tensor:
-    """ Uses the encodings of a perception network
-        (E.g. InceptionNet net or AlexNet)
-        to calculate the loss between
-        prediction and target.
+    """ Uses the encodings of a pre-trained perception network (E.g. InceptionNet net or AlexNet)
+        to calculate the loss between prediction and target.
     """
     assert target.shape == prediction.shape
     N, T = target.shape[0], target.shape[1]
-    # alpha = 0.1
-    pred_encoding = perception_net(prediction.view((N*T, *prediction.shape[2:])))
-    tar_encoding = perception_net(target.view((N*T, *target.shape[2:])))
-    return F.mse_loss(input=pred_encoding, target=tar_encoding, reduction='mean') * 0.5
-    # return torch.mean(alpha * torch.sqrt(torch.norm(pred_encoding - tar_encoding, 2)))
+    prediction_encoding = perception_net(prediction.view((N*T, *prediction.shape[2:])))
+    target_encoding = perception_net(target.view((N*T, *target.shape[2:])))
+    return F.mse_loss(input=prediction_encoding, target=target_encoding, reduction='sum') * 0.5
