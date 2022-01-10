c08c21f14b9462fec45fb04e8373dcca007d35ac
yannik <yannik_phil.frisch@stud.tu-darmstadt.de>
2022-01-10 13:25:06 +0100

    More scripts for hp search

--- scripts/transporter_human36m_training.py
+++ scripts/transporter_human36m_training.py
@@ -4,9 +4,8 @@ import yaml
 from torchvision import transforms
 
 from src.agents.transporter_agent import TransporterAgent
-from src.utils.argparse import parse_arguments
 from src.data.video_dataset import VideoFrameDataset, ImglistToTensor
-
+from src.utils.argparse import parse_arguments
 
 if __name__ == "__main__":
 
@@ -21,29 +20,21 @@ if __name__ == "__main__":
         else:
             transporter_conf['log_dir'] = transporter_conf['log_dir']+f"/{args.id}/"
         print(transporter_conf['log_dir'])
-        transporter_conf['data']['path'] = args.data
 
-    # Apply any number of torchvision transforms here as pre-processing
     preprocess = transforms.Compose([
-        # NOTE: The first transform already converts the image range to (0, 1)
-        ImglistToTensor(),
-        #transforms.RandomChoice([
-        #    transforms.RandomHorizontalFlip(p=0.5),
-        #    transforms.RandomVerticalFlip(p=0.5),
-        #    transforms.RandomApply([transforms.RandomRotation(degrees=90)], p=0.3),
-        #]),
-    ])
+            ImglistToTensor(),
+        ])
 
     data_set = VideoFrameDataset(
-        root_path=args.data,
-        annotationfile_path=os.path.join(args.data, 'annotations.txt'),
-        num_segments=1,
-        frames_per_segment=transporter_conf['model']['n_frames'],
-        imagefile_template='img_{:05d}.jpg',
-        transform=preprocess,
-        random_shift=True,
-        test_mode=True
-    )
+            root_path=args.data,
+            annotationfile_path=os.path.join(args.data, 'annotations.txt'),
+            num_segments=1,
+            frames_per_segment=transporter_conf['model']['n_frames'],
+            imagefile_template='img_{:05d}.jpg',
+            transform=preprocess,
+            random_shift=True,
+            test_mode=False
+        )
 
     transporter_agent = TransporterAgent(dataset=data_set, config=transporter_conf)
 
--- scripts/transporter_manipulator_evaluation.py
+++ scripts/transporter_manipulator_evaluation.py
@@ -3,19 +3,21 @@ import yaml
 
 import torch
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 from src.agents.transporter_agent import TransporterAgent
 from src.data.video_dataset import VideoFrameDataset, ImglistToTensor
 from src.utils.argparse import parse_arguments
-from src.utils.visualization import play_series_and_reconstruction_with_keypoints
+from src.utils.visualization import play_series_and_reconstruction_with_keypoints, play_series_with_keypoints,\
+    plot_keypoint_amplitudes
+from src.losses.kpt_metrics import get_image_patches, tracking_metric, visual_difference_metric, distribution_metric
 
 
 if __name__ == "__main__":
 
     args = parse_arguments()
     # NOTE: Edit config here
-    args.config = "/home/yannik/vssil/results/transporter_manipulator/2021_12_6_13_31/config.yml"
+    args.config = "/home/yannik/vssil/results/transporter_manipulator/2022_1_7_20_11/config.yml"
 
     with open(args.config, 'r') as stream:
         transporter_conf = yaml.safe_load(stream)
@@ -28,6 +30,7 @@ if __name__ == "__main__":
         print(transporter_conf['log_dir'])
         transporter_conf['multi_gpu'] = False
         transporter_conf['device'] = 'cpu'
+        transporter_conf['model']['n_frames'] = 2
 
     # Apply any number of torchvision transforms here as pre-processing
     preprocess = transforms.Compose([
@@ -47,17 +50,20 @@ if __name__ == "__main__":
         test_mode=True
     )
 
+    data_set = Subset(data_set, indices=[100])
+
     eval_data_loader = DataLoader(
         dataset=data_set,
         batch_size=1,
-        shuffle=False
+        # shuffle=False
+        shuffle=True
     )
 
     transporter_agent = TransporterAgent(dataset=data_set, config=transporter_conf)
     transporter_agent.eval_data_loader = eval_data_loader
     # NOTE: Edit checkpoint here
     transporter_agent.load_checkpoint(
-        "/home/yannik/vssil/results/transporter_manipulator/2021_12_6_13_31/checkpoints/chckpt_f0_e60.PTH"
+        "/home/yannik/vssil/results/transporter_manipulator/2022_1_7_20_11/checkpoints/chckpt_f0_e135.PTH"
     )
 
     with torch.no_grad():
@@ -84,13 +90,30 @@ if __name__ == "__main__":
                 # Adapt to visualization
                 key_point_coordinates[..., 1] *= -1
 
-                samples = _sample.unsqueeze(1) if samples is None else torch.cat([samples, _sample.unsqueeze(1)], dim=1)
+                #samples = _sample.unsqueeze(1) if samples is None else torch.cat([samples, _sample.unsqueeze(1)], dim=1)
+                samples = sample[:, t:t+1, ...] if samples is None \
+                    else torch.cat([samples, sample[:, t:t+1, ...]], dim=1)
                 reconstructions = reconstruction.unsqueeze(1) if reconstructions is None \
                     else torch.cat([reconstructions, reconstruction.unsqueeze(1)], dim=1)
                 key_points = key_point_coordinates.unsqueeze(1) if key_points is None \
                     else torch.cat([key_points, key_point_coordinates.unsqueeze(1)], dim=1)
+            
+            play_series_with_keypoints(image_series=samples,
+                                       keypoint_coords=key_points,
+                                       key_point_trajectory=True)
+            plot_keypoint_amplitudes(keypoint_coordinates=key_points,
+                                     target_path=".")
 
-            play_series_and_reconstruction_with_keypoints(image_series=samples,
-                                                          reconstruction=reconstructions,
-                                                          keypoint_coords=key_points)
+            patches = get_image_patches(image_sequence=samples, kpt_sequence=key_points,
+                                        patch_size=(16, 16))
+
+            M_tracking = tracking_metric(patches)
+            M_visual = visual_difference_metric(patches)
+            M_distribution = distribution_metric(key_points, (16, 16))
+
+            with open('metrics.txt', 'w') as metrics_log:
+                metrics_log.write(f"M_tracking: {M_tracking}\n")
+                metrics_log.write(f"M_visual: {M_visual}\n")
+                metrics_log.write(f"M_distribution: {M_distribution}\n")
+
             exit()
