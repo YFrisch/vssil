55990a5103522abb8b7e7d117486173c92d33361
yannik <yannik_phil.frisch@stud.tu-darmstadt.de>
2022-01-22 20:23:14 +0100

    HP scripts

---
+++ src/configs/ulosd/manipulator/make_tune_configs.py
@@ -0,0 +1,32 @@
+""" This script creates individual, counted config files under ./tuned/
+    for each parameter combination in the loops.
+"""
+
+import yaml
+import os
+
+os.makedirs("./tune/", exist_ok=False)
+
+counter = 0
+
+with open('ulosd_manipulator_vanilla_128pix.yml', 'r') as stream:
+    ulosd_conf = yaml.safe_load(stream)
+
+ulosd_conf['log_dir'] = 'results/ulosd_manipulator_vanilla_128pix_tune/'
+
+for n_frames in [4, 8]:
+    for n_init_filters in [8, 16, 32]:
+        for sig in [1.5]:
+            for n_convs in [1, 2]:
+                for fmap_width in [64, 32]:
+                    for init_lr in [0.1, 0.01, 0.001]:
+                        ulosd_conf['model']['n_frames'] = n_frames
+                        ulosd_conf['model']['n_init_filters'] = n_init_filters
+                        ulosd_conf['model']['feature_map_gauss_sigma'] = sig
+                        ulosd_conf['model']['n_convolutions_per_res'] = n_convs
+                        ulosd_conf['model']['feature_map_width'] = fmap_width
+                        ulosd_conf['model']['feature_map_height'] = fmap_width
+                        ulosd_conf['training']['initial_lr'] = init_lr
+                        with open(f'tune/{counter}.yml', 'w') as stream:
+                            yaml.dump(ulosd_conf, stream)
+                        counter += 1
--- tests/unittests/ulosd_unittests.py
+++ tests/unittests/ulosd_unittests.py
@@ -5,6 +5,8 @@ import numpy as np
 
 from src.models.ulosd_layers import add_coord_channels, FeatureMapsToKeyPoints,\
     KeyPointsToFeatureMaps
+from src.models.ulosd_encoders import make_encoder, make_appearance_encoder
+from src.models.ulosd_decoder import make_decoder
 
 
 class OpsTest(unittest.TestCase):
@@ -107,7 +109,85 @@ class KeyPointsToMapsTest(unittest.TestCase):
 class VisionTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.config = {
+            'model': {
+                'n_init_filters': 32,
+                'n_convolutions_per_res': 1,
+                'conv_kernel_size': 3,
+                'feature_map_width': 16,
+                'feature_map_height': 16,
+                'n_feature_maps': 3,
+                'encoder_hidden_activations': 'LeakyReLU',
+                'decoder_hidden_activations': 'LeakyReLU',
+            },
+            'training': {
+                'batch_size': 4
+            }
+        }
+        self.input_shape = (self.config['training']['batch_size'], 2, 3, 64, 64)  # (N, T, C, H, W)
+        self.enc, self.enc_inp_shape, self.enc_out_ch = make_encoder(self.input_shape, self.config)
+        self.dec = make_decoder(self.enc_inp_shape, self.enc_out_ch, self.config)
+        self.app_enc = make_appearance_encoder(self.input_shape, self.config)
+        self.fmaps2kpts = FeatureMapsToKeyPoints()
+        self.kpts2gmaps = KeyPointsToFeatureMaps(sigma=1.5, heatmap_width=16)
+
+    def test_image_encoder_shapes(self):
+        images = torch.zeros((self.config['training']['batch_size'], 3 + 2, 64, 64))
+        kpt_feature_maps = self.enc(images)
+        np.testing.assert_array_equal(
+            x=tuple(kpt_feature_maps.shape),
+            y=(self.config['training']['batch_size'], self.config['model']['n_feature_maps'],
+               self.config['model']['feature_map_height'], self.config['model']['feature_map_width'])
+        )
+
+    def test_fmaps_to_kpts_shapes(self):
+        images = torch.zeros(self.config['training']['batch_size'], 3 + 2, 64, 64)
+        kpt_heatmaps = self.enc(images)
+        kpt_coordinates = self.fmaps2kpts(kpt_heatmaps)
+        np.testing.assert_array_equal(
+            x=tuple(kpt_coordinates.shape),
+            y=(self.config['training']['batch_size'], self.config['model']['n_feature_maps'], 3)
+        )
+
+    def test_kpts_to_gmaps_shapes(self):
+        kpts = torch.zeros(self.config['training']['batch_size'],
+                           self.config['model']['n_feature_maps'],
+                           3)
+        gmaps = self.kpts2gmaps(kpts)
+        np.testing.assert_array_equal(
+            x=tuple(gmaps.shape),
+            y=(self.config['training']['batch_size'], self.config['model']['n_feature_maps'],
+               self.config['model']['feature_map_height'], self.config['model']['feature_map_width'])
+        )
+
+    def test_image_decoder_shapes(self):
+
+        first_frame = torch.zeros(self.config['training']['batch_size'],
+                                  3, 64, 64)
+        first_frame_fmaps = self.app_enc(first_frame)
+        first_frame_kpts = torch.zeros(self.config['training']['batch_size'],
+                                       self.config['model']['n_feature_maps'], 3)
+        current_frame_kpts = torch.zeros(self.config['training']['batch_size'],
+                                         self.config['model']['n_feature_maps'], 3)
+
+        first_frame_gmaps = self.kpts2gmaps(first_frame_kpts)
+        current_frame_gmaps = self.kpts2gmaps(current_frame_kpts)
+
+        combined_representation = torch.cat([current_frame_gmaps, first_frame_fmaps, first_frame_gmaps], dim=1)
+        combined_representation = add_coord_channels(combined_representation)
+
+        np.testing.assert_array_equal(
+            x=tuple(combined_representation.shape),
+            y=(self.config['training']['batch_size'],
+               2 * self.config['model']['n_feature_maps'] + self.enc_out_ch + 2,
+               self.config['model']['feature_map_height'], self.config['model']['feature_map_width'])
+        )
+
+        rec_input = self.dec(combined_representation)
+        np.testing.assert_array_equal(
+            x=tuple(rec_input.shape),
+            y=(self.config['training']['batch_size'], 3, 64, 64)
+        )
 
 
 if __name__ == "__main__":
